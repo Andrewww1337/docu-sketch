b8765eb94e45ea4f09e20da79853ac22356a1274
Andrewww1337 <93862187+Andrewww1337@users.noreply.github.com>
2023-10-18 23:19:02 +0300

    feat: add different way icon dimonstration

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -4,8 +4,14 @@ import { faNames } from "../../fa-names";
 
 import "./button.css";
 
-export const Button = ({ iconsArray, setIconsArray }) => {
-  const [icons, setIcons] = useState([]);
+export const Button = ({
+  iconsArray,
+  setIconsArray,
+  arrayForOneIconDemo,
+  setArrayForOneIconDemo,
+}) => {
+  const [icons, setIcons] = useState(null);
+  const [alloneIcon, setAlloneIcon] = useState(null);
 
   const setNewIcon = () => {
     setIcons({
@@ -13,15 +19,31 @@ export const Button = ({ iconsArray, setIconsArray }) => {
     });
   };
 
+  const setOneIcon = () => {
+    setAlloneIcon({
+      id: Math.floor(Math.random() * faNames?.length),
+    });
+  };
+
   useEffect(() => {
-    setIconsArray([...iconsArray, icons]);
+    if (icons) {
+      setIconsArray([...iconsArray, icons]);
+    }
   }, [icons]);
+  useEffect(() => {
+    if (alloneIcon) {
+      setArrayForOneIconDemo([...arrayForOneIconDemo, alloneIcon]);
+    }
+  }, [alloneIcon]);
 
   return (
     <button
       className="getIconButton"
       type="button"
-      onClick={() => setTimeout(setNewIcon, 3000)}
+      onClick={() => {
+        setOneIcon();
+        setTimeout(setNewIcon, 3000);
+      }}
     >
       Жмяк
     </button>
--- src/components/iconGenerator/iconGenerator.jsx
+++ src/components/iconGenerator/iconGenerator.jsx
@@ -1,16 +1,47 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button } from "../button";
 import { IconList } from "../iconList";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faNames } from "../../fa-names";
 
 import "./iconGenerator.css";
 
 export const IconGenerator = () => {
   const [iconsArray, setIconsArray] = useState([]);
+  const [arrayForOneIconDemo, setArrayForOneIconDemo] = useState([]);
+  const removeIcon = () => {
+    const removedArray = [...arrayForOneIconDemo];
+    const firstElement = removedArray.shift();
+    setArrayForOneIconDemo(removedArray);
+  };
+  useEffect(() => {
+    if (arrayForOneIconDemo[0]) {
+      setTimeout(removeIcon, 3000);
+    }
+  }, [arrayForOneIconDemo]);
 
   return (
     <div className="iconGenerator">
-      <Button iconsArray={iconsArray} setIconsArray={setIconsArray} />
-      <IconList iconsArray={iconsArray} />
+      <div className="oneIconDemo">
+        {arrayForOneIconDemo[0] && (
+          <FontAwesomeIcon
+            className="icon"
+            icon={faNames[arrayForOneIconDemo[0]?.id]}
+            size="xl"
+          />
+        )}
+      </div>
+
+      <Button
+        iconsArray={iconsArray}
+        setIconsArray={setIconsArray}
+        arrayForOneIconDemo={arrayForOneIconDemo}
+        setArrayForOneIconDemo={setArrayForOneIconDemo}
+      />
+      <IconList
+        iconsArray={iconsArray}
+        arrayForOneIconDemo={arrayForOneIconDemo}
+      />
     </div>
   );
 };
